c9375a149b5f78c403b72387d3713442112cdd62
Biser Damyanovski <biserdam@gmail.com>
2018-06-16 07:40:31 +0300

    Initial Commit (Libraries added)

--- Pickaxe.Faucet.ConsoleApp/Pickaxe.Faucet.ConsoleApp.cs
+++ Pickaxe.Faucet.ConsoleApp/Pickaxe.Faucet.ConsoleApp.cs
@@ -1,4 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Text;
+using System.Linq;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Asn1.Sec;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Crypto.Digests;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Asn1.Ocsp;
+using Org.BouncyCastle.Crypto.Signers;
 
 namespace Pickaxe.Faucet.ConsoleApp
 {
